e7c87cb1287193f184ae4d9e7be298426998f51c
Renaud <renaud.melet@epitech.eu>
2020-04-06 13:35:02 +0200

    Add many options

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 mod utils;
 
+use wasm_bindgen::prelude::*;
+
 #[macro_use]
 extern crate lazy_static;
 extern crate serde_derive;
+use utils::*;
 
+use js_sys::RegExp;
 use std::sync::Mutex;
-use std::collections::HashMap;
-
-use serde_json::json;
-use wasm_bindgen::prelude::*;
+use serde_json::{json, Value};
 use pulldown_cmark::{Parser, Options, html, Event, Tag, CowStr};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -18,134 +19,248 @@ use pulldown_cmark::{Parser, Options, html, Event, Tag, CowStr};
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 lazy_static! {
-    static ref OPTIONS: Mutex<HashMap<String, serde_json::Value>> = {
-        let mut m = HashMap::new();
-        m.insert("headerLevelStart".to_string(), json!(false));
-        m.insert("literalMidWordAsterisks".to_string(), json!(false));
-        m.insert("noHeaderId".to_string(), json!(false));
-        m.insert("simpleLineBreaks".to_string(), json!(false));
-        m.insert("strikethrough".to_string(), json!(false));
-        m.insert("tables".to_string(), json!(false));
-        m.insert("tasklists".to_string(), json!(false));
+    static ref OPTIONS: Mutex<Value> = {
+        let m = json!({
+            "customizedHeaderId": false,
+            "emoji": false,
+            "ghCompatibleHeaderId": false,
+            "ghMentions": false,
+            "ghMentionsLink": "https://github.com/{u}",
+            "headerLevelStart": 1,
+            "literalMidWordAsterisks": false,
+            "noHeaderId": false,
+            "openLinksInNewWindow": false,
+            "prefixHeaderId": false,
+            "rawHeaderId": false,
+            "rawPrefixHeaderId": false,
+            "requireSpaceBeforeHeadingText": false,
+            "simpleLineBreaks": false,
+            "strikethrough": false,
+            "tables": false,
+            "tasklists": false
+        });
         Mutex::new(m)
     };
 }
 
 #[wasm_bindgen]
-pub fn getOption(key: &str) -> JsValue {
+pub fn get_option(key: &str) -> JsValue {
     JsValue::from_serde(&OPTIONS.lock().unwrap()[key]).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn getOptions() -> JsValue {
-    JsValue::from_serde(&OPTIONS.lock().unwrap().to_owned()).unwrap()
+pub fn get_options() -> JsValue {
+    JsValue::from_serde(&OPTIONS.lock().unwrap().as_object().unwrap()).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn setOption(key: String, value: JsValue) {
-    OPTIONS.lock().unwrap().insert(key, value.into_serde().unwrap());
+pub fn set_option(key: String, value: JsValue) {
+    OPTIONS.lock().unwrap()[key] = value.into_serde().unwrap();
+}
+
+fn add_link_to_mentions(text: &str, options: &serde_json::Value) -> String {
+    let re = RegExp::new(r"\@[a-zA-Z][0-9a-zA-Z_]*", "gi");
+    let mut ret = String::from(text);
+
+    let mut result = re.exec(text);
+
+    while result.is_some() {
+        let mut v = vec![];
+        result.unwrap().for_each(&mut |x, _, _| v.push(x));
+
+        let href = if parse_bool(&options["openLinksInNewWindow"]) {
+            "<a href=\"{link}\" rel=\"noopener noreferrer\" target=\"_blank\">{mention}</a>"
+        } else {
+            "<a href=\"{link}\">{mention}</a>"
+        };
+        let new_link = options["ghMentionsLink"].as_str().unwrap();
+
+        for mention in v {
+            let m = mention.as_string().unwrap();
+            let complete_href = href.replace("{link}", new_link).replace("{u}", m.split_at(1).1).replace("{mention}", m.as_str());
+
+            ret = ret.replace(m.as_str(), complete_href.as_str());
+        }
+        result = re.exec(text);
+    }
+    ret
+}
+
+fn handle_id(text: &mut CowStr, options: &serde_json::Value) -> String {
+    let mut id = text.clone().into_string();
+
+    if parse_bool(&options["customizedHeaderId"]) {
+        let result = RegExp::new(r"\s?\{([^{]+?)}\s*$", "gm").exec(text);
+
+        if result.is_some() {
+            let arr = result.unwrap();
+            *text = CowStr::from(text.replace(arr.get(0).as_string().unwrap().as_str(), ""));
+            id = arr.get(1).as_string().unwrap();
+        }
+    }
+
+    if parse_bool(&options["noHeaderId"]) {
+        return String::new();
+    }
+
+    let prefix = if parse_bool(&options["prefixHeaderId"]) && options["prefixHeaderId"].is_boolean() {
+        "section-"
+    } else if parse_bool(&options["prefixHeaderId"]) && options["prefixHeaderId"].is_string() {
+        options["prefixHeaderId"].as_str().unwrap()
+    } else {
+        ""
+    };
+
+    let mut title = if !parse_bool(&options["rawPrefixHeaderId"]) {
+        format!("{}{}", prefix, id)
+    } else {
+        id
+    };
+
+    title = if parse_bool(&options["ghCompatibleHeaderId"]) {
+        let new_title = title.replace(" ", "-");
+        regex_replace(new_title.as_str(), "[&+$,/:;=?@\"#{}|^¨~[]`\\*)(%.!'<>]", "", "g").to_lowercase()
+    } else if parse_bool(&options["rawHeaderId"]) {
+        regex_replace(title.as_str(), "[\"\' ]", "-", "g").to_lowercase()
+    } else {
+        regex_replace(title.as_str(), r"[^\w]", "", "g").to_lowercase()
+    };
+
+    if parse_bool(&options["rawPrefixHeaderId"]) {
+        title = format!("{}{}", prefix, title);
+    };
+
+    format!(" id=\"{}\"", title)
+}
+
+fn handle_header(text: &mut CowStr, hlevel: u32, options: &serde_json::Value) -> String {
+    let hid = handle_id(text, options);
+
+    format!("<h{}{}>{}</h{}>", hlevel, hid, text, hlevel)
 }
 
 #[wasm_bindgen]
-pub struct Converter {
-    options: HashMap<String, serde_json::Value>,
-}
+pub fn make_html(content: &str, options: &JsValue) -> String {
+    let options: serde_json::Value = options.into_serde().unwrap();
+    let mut cmark_options = Options::empty();
 
-#[wasm_bindgen]
-impl Converter {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Converter {
-        Converter { options: OPTIONS.lock().unwrap().clone() }
+    cmark_options.insert(Options::ENABLE_FOOTNOTES);
+    if parse_bool(&options["strikethrough"]) {
+        cmark_options.insert(Options::ENABLE_STRIKETHROUGH);
+    }
+    if parse_bool(&options["tables"]) {
+        cmark_options.insert(Options::ENABLE_TABLES);
+    }
+    if parse_bool(&options["tasklists"]) {
+        cmark_options.insert(Options::ENABLE_TASKLISTS);
     }
 
-    pub fn setOption(&mut self, key: String, value: JsValue)
-    {
-        self.options.insert(key, value.into_serde().unwrap());
-    }
+    let mut parse_event : Option<Event> = None;
+    let parser = Parser::new_ext(content, cmark_options);
 
-    pub fn getOption(&self, key: &str) -> JsValue {
-        JsValue::from_serde(&self.options[key]).unwrap()
-    }
+    let parser = parser.filter_map(|event|
+        match event {
+            Event::Start(Tag::Heading(level @ 1..=6)) => {
+                let header_level_start = parse_int(&options["headerLevelStart"]);
 
-    pub fn getOptions(&self) -> JsValue {
-        JsValue::from_serde(&self.options).unwrap()
-    }
-
-    pub fn makeHtml(&self, content: &str) -> String {
-        let mut options = Options::empty();
-
-        options.insert(Options::ENABLE_FOOTNOTES);
-        if self.options.contains_key("strikethrough") && self.options["strikethrough"].is_boolean() && self.options["strikethrough"].as_bool().unwrap() {
-            options.insert(Options::ENABLE_STRIKETHROUGH);
-        }
-        if self.options.contains_key("tables") && self.options["tables"].is_boolean() && self.options["tables"].as_bool().unwrap() {
-            options.insert(Options::ENABLE_TABLES);
-        }
-        if self.options.contains_key("tasklists") && self.options["tasklists"].is_boolean() && self.options["tasklists"].as_bool().unwrap() {
-            options.insert(Options::ENABLE_TASKLISTS);
-        }
-
-        let parser = Parser::new_ext(content, options);
-
-        let mut heading_level: Option<i64> = None;
-
-        let parser = parser.filter_map(| event |
-            match event {
-                Event::Start(Tag::Heading(level @1..=6)) => {
-                    if self.options.contains_key("noHeaderId") && self.options["noHeaderId"].is_boolean() && self.options["noHeaderId"].as_bool().unwrap() {
-                        return Some(Event::Start(Tag::Heading(level)))
-                    }
-                    heading_level = Some(level as i64);
-                    if self.options.contains_key("headerLevelStart") && self.options["headerLevelStart"].is_number() {
-                        heading_level = Some(self.options["headerLevelStart"].as_i64().unwrap() + level as i64 - 1);
-                    }
-                    None
-                },
-
-                Event::Text(text) => {
-                    if heading_level.is_some() {
-                        //val.trim().toLowerCase().replace(/[^\w\- ]+/g, ' ').replace(/\s+/g, '-').replace(/\-+$/, '');
-                        let anchor = text.clone().into_string().trim().to_lowercase().replace(" ", "-");
-                        return Event::Html(CowStr::from(format!("<h{} id=\"{}\">{}</h{}>", heading_level.unwrap(), anchor, text, heading_level.unwrap()))).into()
-                    }
-                    Some(Event::Text(text))
-                },
-
-                Event::End(Tag::Heading(level)) => {
-                    if self.options.contains_key("noHeaderId") && self.options["noHeaderId"].is_boolean() && self.options["noHeaderId"].as_bool().unwrap() {
-                        return Some(Event::End(Tag::Heading(level)))
-                    }
-                    heading_level = None;
-                    Some(Event::SoftBreak)
-                },
-
-                Event::Start(Tag::Strong) => {
-                    if self.options.contains_key("literalMidWordAsterisks") && self.options["literalMidWordAsterisks"].is_boolean() && self.options["literalMidWordAsterisks"].as_bool().unwrap() {
-                        return Some(Event::Text(CowStr::from("**")))
-                    }
-                    Some(Event::Start(Tag::Strong))
-                },
-
-                Event::End(Tag::Strong) => {
-                    if self.options.contains_key("literalMidWordAsterisks") && self.options["literalMidWordAsterisks"].is_boolean() && self.options["literalMidWordAsterisks"].as_bool().unwrap() {
-                        return Some(Event::Text(CowStr::from("**")))
-                    }
-                    Some(Event::End(Tag::Strong))
-                },
-
-                Event::SoftBreak => {
-                    if self.options.contains_key("simpleLineBreaks") && self.options["simpleLineBreaks"].is_boolean() && self.options["simpleLineBreaks"].as_bool().unwrap() {
-                        return Some(Event::HardBreak)
-                    }
-                    Some(Event::SoftBreak)
-                },
-
-                _ => Some(event),
+                parse_event = if header_level_start.is_some() {
+                    Some(Event::Start(Tag::Heading(header_level_start.unwrap() as u32 + level - 1)))
+                } else {
+                    Some(Event::Start(Tag::Heading(level)))
+                };
+                None
             }
-        );
 
-        let mut html_output = String::new();
-        html::push_html(&mut html_output, parser);
-        html_output
-    }
+            Event::Start(Tag::Link(link_type, destination, url)) => {
+                if parse_bool(&options["openLinksInNewWindow"]) {
+                    parse_event = Some(Event::Start(Tag::Link(link_type, destination, url)));
+                    None
+                } else {
+                    Some(Event::Start(Tag::Link(link_type, destination, url)))
+                }
+            }
+
+            Event::Text(mut text) => {
+                let mut html = false;
+
+                match &parse_event {
+                    Some(Event::Start(Tag::Heading(level))) => {
+                        text = CowStr::from(handle_header(&mut text, *level, &options));
+                        html = true;
+                    },
+                    Some(Event::Start(Tag::Link(_, destination, _))) => {
+                        text = CowStr::from(format!("<a href=\"{}\" rel=\"noopener noreferrer\" target=\"_blank\">{}</a>", destination, text));
+                        html = true;
+                        parse_event = None;
+                    },
+                    None => {
+                        if !parse_bool(&options["requireSpaceBeforeHeadingText"]) && text.contains("#") {
+                            let count = text.chars().take_while(|x| x == &'#').count() as u32;
+                            text = CowStr::from(text.replace("#", ""));
+
+                            let level = match parse_int(&options["headerLevelStart"]) {
+                                None => {
+                                    count
+                                },
+                                Some(level) => {
+                                    level as u32 + count - 1
+                                },
+                            };
+                            text = CowStr::from(handle_header(&mut text, level, &options));
+                            html = true;
+
+                        }
+                    },
+                    Some(_) => (),
+                }
+
+                if parse_bool(&options["emoji"]) {
+                    //https://github.com/showdownjs/showdown/wiki/Emojis
+                    text = CowStr::from(gh_emoji::Replacer::new().replace_all(&text).into_owned());
+                }
+
+                if text.find("@").is_some() && parse_bool(&options["ghMentions"]) {
+                    text = CowStr::from(add_link_to_mentions(&text, &options));
+                    html = true;
+                }
+
+                if html {
+                    Some(Event::Html(text))
+                } else {
+                    Some(Event::Text(text))
+                }
+            }
+
+            Event::End(Tag::Heading(_)) => {
+                parse_event = None;
+                Some(Event::SoftBreak)
+            }
+
+            Event::Start(Tag::Strong) => {
+                if parse_bool(&options["literalMidWordAsterisks"]) {
+                    return Some(Event::Text(CowStr::from("**")));
+                }
+                Some(Event::Start(Tag::Strong))
+            }
+
+            Event::End(Tag::Strong) => {
+                if parse_bool(&options["literalMidWordAsterisks"]) {
+                    return Some(Event::Text(CowStr::from("**")));
+                }
+                Some(Event::End(Tag::Strong))
+            }
+
+            Event::SoftBreak => {
+                if parse_bool(&options["simpleLineBreaks"]) {
+                    return Some(Event::HardBreak);
+                }
+                Some(Event::SoftBreak)
+            }
+            _ => Some(event),
+        }
+    );
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    html_output
 }
